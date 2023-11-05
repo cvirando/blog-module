a7cedc290f05f58a13af7fe479b5c03150d90846
robertnicjoo <robertnicjoo@outlook.com>
2023-11-05 13:43:55 +0700

    fixed routing issue

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -25,9 +25,6 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <a class="btn btn-success btn-block my-2" href="{{route('blogPostsCreate')}}">Settigns</a>
-        </div>
         @forelse ($posts as $post)
         <div class="col-md-4">
             <div class="card">
